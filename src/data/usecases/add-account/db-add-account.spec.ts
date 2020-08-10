0851b7281652f9bc597b9448c01a215fd40bae1d
randal923 <randal.andrade@hotmail.com>
2020-08-09 22:47:59 -0700

    refactor: move encrypter  to factory

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -5,13 +5,17 @@ interface SutTypese {
   sut: DbAddAccount
   encrypterStub: Encrypter
 }
-const makeSut = (): any => {
-  class EncrypterStub {
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
     async encrypt (value: string): Promise<string> {
       return await new Promise(resolve => resolve('hashed_password'))
     }
   }
-  const encrypterStub = new EncrypterStub()
+  return new EncrypterStub()
+}
+const makeSut = (): any => {
+  const encrypterStub = makeEncrypter()
   const sut = new DbAddAccount(encrypterStub)
 
   return {
