95c5ad036d650669e79d1e927c2c67b4222d2eb1
randal923 <randal.andrade@hotmail.com>
2020-08-19 17:51:15 -0700

    feat: signup with tests

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setUpRoutes from './routes'
 
 const app = express()
 setupMiddlewares(app)
-
+setUpRoutes(app)
 export default app
