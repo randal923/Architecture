1c3c5e287a84e009385f6802650c4d9a9650e246
randal923 <randal.andrade@hotmail.com>
2020-08-19 23:10:36 -0700

    feat: ensure mongodb reconnect if client is down

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
